b18a6068e1c464148cab6c80799cf2ac7a57c205
Ubuntu <ubuntu@ip-172-31-20-93.us-west-2.compute.internal>
2014-09-24 21:05:35 +0000

    front end

--- front_end/app/views.py
+++ front_end/app/views.py
@@ -9,23 +9,26 @@ import random
 import pandas as pd
 import numpy as np
 
-import sklearn
-from sklearn import preprocessing
-from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
-from sklearn import metrics
-from sklearn.cross_validation import cross_val_score
-from sklearn.feature_extraction import DictVectorizer
-from sklearn_pandas import DataFrameMapper, cross_val_score
+#import sklearn
+#from sklearn import preprocessing
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.cross_validation import train_test_split
+#from sklearn import metrics
+#from sklearn.cross_validation import cross_val_score
+#from sklearn.feature_extraction import DictVectorizer
+#from sklearn_pandas import DataFrameMapper, cross_val_score
+#
+#from patsy import dmatrices
 
-from patsy import dmatrices
+#db = mdb.connect(user="root", host="localhost", db="gamelogdb", charset='utf8')  #db="world_innodb", charset='utf8')
 
-db = mdb.connect(user="root", host="localhost", db="gamelogdb", charset='utf8')  #db="world_innodb", charset='utf8')
+db = mdb.connect(user="root", host="localhost", db="nbagamedb", charset='utf8')
+#db = mdb.connect(user="root", host="localhost", db="demodb", charset='utf8')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html",
+    return render_template("index_final.html",
         title = 'Home', user = { 'nickname': 'Miguel' },
         )
 
@@ -33,70 +36,130 @@ def index():
 @app.route('/regression')
 def regression():
     """Add two numbers server side, ridiculous but well..."""
-    pts = request.args.get('a', 0, type=float)
-    ast = request.args.get('b', 0, type=float)
-    reb = request.args.get('c', 0, type=float)
-    n = request.args.get('d', 0, type=float)
-    #n = 100 #request.args.get('NGAMES', 0, type=float)
+    p_name = request.args.get('d', 0, type=str)
+    print p_name  
+
+    d1 = ''
+    d2 = ''
+    d3 = ''
+
+    n1 = ''
+    n2 = ''
+    n3 = ''
 
     df = {}
-
     cur = {}
+    p_profile = "player_profile"
+    cur[p_profile]  = db.cursor(mdb.cursors.DictCursor)
+    cur[p_profile].execute("SELECT FULLNAME,URL,PERSONID FROM rookie_profile;")
+    df[p_profile] = pd.DataFrame( cur[p_profile].fetchall() )
 
-    prob = []
-    train_s = []
-    train_o = []
-    mis_r = []
+    #print df[p_profile]
 
-    sig_str = 'sig_%dg' % n
-    bkg_str = 'bkg_%dg' % n
+    url = ""
+    for i, row in df[p_profile].iterrows():
+        if p_name == row["FULLNAME"]:
+            url = row["URL"]
+            pid = row["PERSONID"]
 
-    cur[sig_str]  = db.cursor(mdb.cursors.DictCursor)
-    cur[sig_str].execute("SELECT PTS,AST,REB FROM star WHERE PLAYED<=%d AND LEARN_INDICATOR=1;" % n)
-    df[sig_str] = pd.DataFrame( cur[sig_str].fetchall() )
-
-    cur[bkg_str]  = db.cursor(mdb.cursors.DictCursor)
-    cur[bkg_str].execute("SELECT PTS,AST,REB FROM non_star WHERE PLAYED<=%d AND LEARN_INDICATOR=1;" % n)
-    df[bkg_str] = pd.DataFrame( cur[bkg_str].fetchall() )
-
-
-    train_sig = {}
-    train_bkg = {}
-    # Signal: use 80% for training, 20% for testing
-    # Background: use sig*5 for training, rest for testing 
-    train_sig[sig_str] = random.sample(df[sig_str].index, int(df[sig_str].shape[0] * 0.6)) #int(df['sig_10g'].shape[0] * 0.8))
-    train_bkg[bkg_str] = random.sample(df[bkg_str].index, len(train_sig[sig_str]))#int(df[bkg_str].shape[0] * 0.6)) #int(df['sig_10g'].shape[0] * 0.8 * 5))
-
-    df[sig_str+'_training'] = df[sig_str].ix[ train_sig[sig_str] ]
-    df[bkg_str+'_training'] = df[bkg_str].ix[ train_bkg[bkg_str] ]
-
-    #df[sig_str+'_testing'] = df[sig_str].drop(train_sig[sig_str])
-    #df[bkg_str+'_intermediate'] = df[bkg_str].drop(train_bkg[bkg_str])
-    #df[bkg_str+'_testing'] = df[bkg_str].ix[ random.sample(df[bkg_str+'_intermediate'].index, int(df[sig_str+'_testing'].shape[0])) ]
-
-    df_sig_train = df[sig_str+'_training']
-    df_bkg_train = df[bkg_str+'_training'] 
-
-    X_sig = np.array(df_sig_train)
-    y_sig = np.array(X_sig.shape[0] * [1])
+    p_frac = "player_fraction"
+    cur[p_frac] = db.cursor(mdb.cursors.DictCursor)
+    cur[p_frac].execute("SELECT Player_ID,Score FROM score;")
+    df[p_frac] = pd.DataFrame( cur[p_frac].fetchall() )
     
-    X_bkg = np.array(df_bkg_train)
-    y_bkg = np.array(X_bkg.shape[0] * [0])
+    size = df[p_frac].shape[0]
+    for index, row in df[p_frac].iterrows():
+        if pid == row["Player_ID"]:
+            p = int((1-float(index+1)/float(size+1))*100.0)
 
 
-    X = np.concatenate((X_sig, X_bkg))
-    y = np.concatenate((y_sig, y_bkg))
+    p_sim = "player_similarity"
+    cur[p_sim] = db.cursor(mdb.cursors.DictCursor)
+    cur[p_sim].execute("SELECT RookieID,FirstDegreeID,SecondDegreeID,ThirdDegreeID,FirstDegreeName,SecondDegreeName,ThirdDegreeName FROM similarity;")
+    df[p_sim] = pd.DataFrame( cur[p_sim].fetchall() )
+    for index, row in df[p_sim].iterrows():
+        if pid == row["RookieID"]:
+            d1 = "http://stats.nba.com/media/players/230x185/"+str(int(row["FirstDegreeID"]))+".png"
+            d2 = "http://stats.nba.com/media/players/230x185/"+str(int(row["SecondDegreeID"]))+".png"
+            d3 = "http://stats.nba.com/media/players/230x185/"+str(int(row["ThirdDegreeID"]))+".png"
+            n1 = row["FirstDegreeName"] 
+            n2 = row["SecondDegreeName"]
+            n3 = row["ThirdDegreeName"]
 
-    model = LogisticRegression()
 
-    model.fit(X, y)
+    #####pts = request.args.get('a', 0, type=float)
+    #####ast = request.args.get('b', 0, type=float)
+    #####reb = request.args.get('c', 0, type=float)
+    #####n = request.args.get('d', 0, type=float)
+    ######n = 100 #request.args.get('NGAMES', 0, type=float)
 
-    i=[float(pts), float(ast), float(reb)]
-    input_n = np.array(i)
-    prob = round(((model.predict_proba(input_n))[0][1])*100,0)
-    conf = round((model.score(X, y))*100,0)
-    return jsonify(result=[prob, conf])
+    #####df = {}
 
+    #####cur = {}
+
+    #####prob = []
+    #####train_s = []
+    #####train_o = []
+    #####mis_r = []
+
+    #####sig_str = 'sig_%dg' % n
+    #####bkg_str = 'bkg_%dg' % n
+
+    #####cur[sig_str]  = db.cursor(mdb.cursors.DictCursor)
+    #####cur[sig_str].execute("SELECT PTS,AST,REB FROM star WHERE PLAYED<=%d AND LEARN_INDICATOR=1;" % n)
+    #####df[sig_str] = pd.DataFrame( cur[sig_str].fetchall() )
+
+    #####cur[bkg_str]  = db.cursor(mdb.cursors.DictCursor)
+    #####cur[bkg_str].execute("SELECT PTS,AST,REB FROM non_star WHERE PLAYED<=%d AND LEARN_INDICATOR=1;" % n)
+    #####df[bkg_str] = pd.DataFrame( cur[bkg_str].fetchall() )
+
+
+    #####train_sig = {}
+    #####train_bkg = {}
+    ###### Signal: use 80% for training, 20% for testing
+    ###### Background: use sig*5 for training, rest for testing 
+    #####train_sig[sig_str] = random.sample(df[sig_str].index, int(df[sig_str].shape[0] * 0.6)) #int(df['sig_10g'].shape[0] * 0.8))
+    #####train_bkg[bkg_str] = random.sample(df[bkg_str].index, len(train_sig[sig_str]))#int(df[bkg_str].shape[0] * 0.6)) #int(df['sig_10g'].shape[0] * 0.8 * 5))
+
+    #####df[sig_str+'_training'] = df[sig_str].ix[ train_sig[sig_str] ]
+    #####df[bkg_str+'_training'] = df[bkg_str].ix[ train_bkg[bkg_str] ]
+
+    ######df[sig_str+'_testing'] = df[sig_str].drop(train_sig[sig_str])
+    ######df[bkg_str+'_intermediate'] = df[bkg_str].drop(train_bkg[bkg_str])
+    ######df[bkg_str+'_testing'] = df[bkg_str].ix[ random.sample(df[bkg_str+'_intermediate'].index, int(df[sig_str+'_testing'].shape[0])) ]
+
+    #####df_sig_train = df[sig_str+'_training']
+    #####df_bkg_train = df[bkg_str+'_training'] 
+
+    #####X_sig = np.array(df_sig_train)
+    #####y_sig = np.array(X_sig.shape[0] * [1])
+    #####
+    #####X_bkg = np.array(df_bkg_train)
+    #####y_bkg = np.array(X_bkg.shape[0] * [0])
+
+
+    #####X = np.concatenate((X_sig, X_bkg))
+    #####y = np.concatenate((y_sig, y_bkg))
+
+    #####model = LogisticRegression()
+
+    #####model.fit(X, y)
+
+    #####i=[float(pts), float(ast), float(reb)]
+    #####input_n = np.array(i)
+    #####prob = round(((model.predict_proba(input_n))[0][1])*100,0)
+    #####conf = round((model.score(X, y))*100,0)
+    #return jsonify(result=[prob, conf])
+    ploc = {}
+    #pid = "static/Gaussian/Gaussian_"+str(pid)+".jpg"
+    ploc['PTS'] = "static/images/Performance/PTS_"+str(pid)+".jpg"
+    ploc['AST'] = "static/images/Performance/AST_"+str(pid)+".jpg"
+    ploc['REB'] = "static/images/Performance/REB_"+str(pid)+".jpg"
+    ploc['MIN'] = "static/images/Performance/MIN_"+str(pid)+".jpg"
+    ploc['FG_PCT'] = "static/images/Performance/FG_PCT_"+str(pid)+".jpg"
+    ploc['EFF'] = "static/images/Performance/EFF_"+str(pid)+".jpg"
+    print url,pid,p,n1,n2,n3
+    return jsonify(result=[url,p, ploc['PTS'], ploc['AST'], ploc['REB'], ploc['MIN'], ploc['FG_PCT'], ploc['EFF'], d1, d2, d3, n1, n2, n3])
 
 
 
